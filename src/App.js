6ac24e01a481e69674c36a61ca1d462a4f93f2e2
githubowyprojekcik <githubowyprojekcik@gmail.com>
2017-11-07 23:13:09 +0100

    presentation

--- src/App.js
+++ src/App.js
@@ -1,44 +1,137 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { 
   BrowserRouter as Router, 
   Route,
   Link  
 } from 'react-router-dom'
 import './App.css'
-import mace from './img/mace.jpg'
-import miecz from './img/miecz.jpg'
-import axe from './img/Axe.jpg'
-
-const Mace = () => <img src={mace} width='200px' height='200px' alt="" />
-
-const Sword = () => <img src={miecz} width='200px' height='200px' alt=""/>
-
-const Axe = () => <img src={axe} width='200px' height='200px' alt="" />
+import mace from './img/mace.png'
+import miecz from './img/miecz.png'
+import axe from './img/Axe.png'
 
 
-const App = () => 
-  
-  <div>
+class Mace extends Component {      
+  constructor(props){
+    super(props)
+    this.state = {
+      currentAngle: 0,
+      anglePerClick: 720,
+    }
+    this.rotateCircle = this.rotateCircle.bind(this);
+  }
+  rotateCircle() {
+    const { currentAngle, anglePerClick } = this.state;
+    this.setState({ 
+      currentAngle: currentAngle + anglePerClick 
+    })
+  }    
+  render() {
+    const circleStyle = {
+      transform: `rotateZ(${this.state.currentAngle}deg)`,
+      cursor: 'pointer',
+      transition: 'transform 3.1s cubic-bezier(0.68, -1.35, 0.265, 1.55)'
+    }  
 
-  <div class="instruction">
-  <div>Pick your weapon</div>
-  </div>
+    return (             
+          <img src={mace} width='200px' height='200px' alt="" 
+               style={circleStyle}
+               onClick={this.rotateCircle}/>
+    );
+  }
+}
 
-  <Router>
-    <div className="container">
+class Sword extends Component {      
+  constructor(props){
+    super(props)
+    this.state = {
+      currentAngle: 0,
+      anglePerClick: 720,
+    }
+    this.rotateCircle = this.rotateCircle.bind(this);
+  }
+  rotateCircle() {
+    const { currentAngle, anglePerClick } = this.state;
+    this.setState({ 
+      currentAngle: currentAngle + anglePerClick 
+    })
+  }    
+  render() {
+    const circleStyle = {
+      transform: `rotateZ(${this.state.currentAngle}deg)`,
+      cursor: 'pointer',
+      transition: 'transform 3.1s cubic-bezier(0.68, -1.35, 0.265, 1.55)'
+    }  
 
-    <div className="weapon-picker">
-      <Link to="/" className="no-underline">Mace</Link>
-      <Link to="/Second" className="no-underline">Sword</Link>
-      <Link to="/Third" className="no-underline">Axe</Link>
-    </div>
-      
+    return (             
+          <img src={miecz} width='200px' height='200px' alt="" 
+               style={circleStyle}
+               onClick={this.rotateCircle}/>
+    );
+  }
+}
+class Axe extends Component {      
+  constructor(props){
+    super(props)
+    this.state = {
+      currentAngle: 0,
+      anglePerClick: 720,
+    }
+    this.rotateCircle = this.rotateCircle.bind(this);
+  }
+  rotateCircle() {
+    const { currentAngle, anglePerClick } = this.state;
+    this.setState({ 
+      currentAngle: currentAngle + anglePerClick 
+    })
+  }    
+  render() {
+    const circleStyle = {
+      transform: `rotateZ(${this.state.currentAngle}deg)`,
+      cursor: 'pointer',
+      transition: 'transform 3.1s cubic-bezier(0.68, -1.35, 0.265, 1.55)'
+    }  
 
-    <Route exact path="/" component={Mace} />
-    <Route path="/Second" component={Sword} />
-    <Route path="/Third" component={Axe} />
-  </div>
-  </Router>
+    return (             
+          <img src={axe} width='200px' height='200px' alt="" 
+               style={circleStyle}
+               onClick={this.rotateCircle}/>
+    );
+  }
+}
 
-  </div>
+
+
+class App extends Component { 
+  render() {
+     return (
+        <div>
+
+        <div class="instruction">
+        <div>Pick your weapon</div>
+        </div>
+
+        <Router>
+          <div className="container">
+
+          <div className="weapon-picker">
+            <Link to="/" className="no-underline">Mace</Link>
+            <Link to="/Second" className="no-underline">Sword</Link>
+            <Link to="/Third" className="no-underline">Axe</Link>
+          </div>
+            
+
+          <Route exact path="/" component={Mace} />
+          <Route path="/Second" component={Sword} />
+          <Route path="/Third" component={Axe} />
+        </div>
+        </Router>
+
+        <div class="instruction">
+        <div>Click for presentation</div>
+        </div>
+
+        </div>
+    )
+  }
+}
 export default App;
